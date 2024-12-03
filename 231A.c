975b534a82e8a7216d5bbdf346e85ef399857ed0
karthikeya <karthikeyapila3@gmail.com>
2024-12-04 01:33:01 +0530

    Team

---
+++ 231A.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c;
+    int sol_count=0;
+    int no_problems;
+    scanf("%d", &no_problems);
+    for(int i=0; i<no_problems; i++){
+        scanf("%d %d %d", &a, &b, &c);
+        if((a+b+c) >= 2){
+            sol_count++;
+        }
+    }
+    printf("%d\n", sol_count);
+}
